48cf086cdf2135f99cc91ca3a7450e46de02a186
Willian <116973434+Willian-Paulino@users.noreply.github.com>
2023-06-26 00:49:04 +0000

    Comentários

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,22 +10,31 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule,CommonModule],
 })
 export class HomePage {
+  // definição de variaveis para lógica da calculadora
   resultado : string = "";
   num : string = "";
+
+  //A função ira exibir os digitos colocados pelo usuário
   exibirCalculo(n : string){
     this.num = this.resultado
     this.resultado = this.num + n
   }
+
+  //A função ira resetar todos os digitos colocados pelo usuário
   reset(){
     this.resultado = "";
   }
+
+  //A função ira digitar o ultimo digito colocado pelo usuário
   del(){
     this.num = this.resultado;
     this.resultado = this.num.substring(0, this.num.length - 1);
   }
+
+  //A função ira realizar todo o calculo que o usuário desejar
   Calcular(){
-    this.num = this.resultado;
+    this.num = this.resultado;        
     this.num != "" ?  this.resultado = eval(this.num) : alert("Erro! Coloque valores na calculadora!");   
-    
   }
+
 }
