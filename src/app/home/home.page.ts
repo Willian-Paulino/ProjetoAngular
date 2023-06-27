10be31710608d6aa93c0cd59e150206ece548b71
Willian <116973434+Willian-Paulino@users.noreply.github.com>
2023-06-27 13:07:50 +0000

    Finalização

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,15 +13,21 @@ export class HomePage {
   // definição de variaveis para lógica da calculadora
   resultado : string = "";
   num : string = "";
+  num2 : string = "";
+  opcaoporcentagem : Boolean = false;
+  infoporcentagem : string = "";
 
   //A função ira exibir os digitos colocados pelo usuário
   exibirCalculo(n : string){
+    this.infoporcentagem = "";
     this.num = this.resultado
     this.resultado = this.num + n
   }
 
   //A função ira resetar todos os digitos colocados pelo usuário
   reset(){
+    this.opcaoporcentagem = false;
+    this.infoporcentagem = "";
     this.resultado = "";
   }
 
@@ -33,8 +39,29 @@ export class HomePage {
 
   //A função ira realizar todo o calculo que o usuário desejar
   Calcular(){
-    this.num = this.resultado;        
-    this.num != "" ?  this.resultado = eval(this.num) : alert("Erro! Coloque valores na calculadora!");   
+    //verificação para ver se o usuário vai desejar ou não ver a porcentagem de um numero
+      if(this.opcaoporcentagem){
+        this.num = this.resultado;
+        this.resultado = eval("this.num2 * this.num / 100");
+        this.opcaoporcentagem = false;
+      } else{
+        this.num = this.resultado;        
+        this.num != "" ?  this.resultado = eval(this.num) : alert("Erro! Coloque valores na calculadora!");
+      }
+  }
+
+  //função usada para recuperar o numero que o usuario deseja ver a porcentagem
+  calcularPorcentagem(){
+    this.num2 = this.resultado;
+    this.infoporcentagem = "Coloque a porcentagem";
+    this.resultado = "";
+    this.opcaoporcentagem = true;
+  }
+
+  //função usada para colocar o primeiro numero que o usuário digitar como negativo
+  calcPosNeg(){
+    this.num = eval("this.resultado * -1");
+    this.resultado = this.num;
   }
 
 }
